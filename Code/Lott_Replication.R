436d35c6050484fedaa0e9ea6e716c98e8089cc5
Tuugboat <robertpetit16@gmail.com>
2022-02-22 13:05:04 -0600

    Checked the state level data as well

--- Code/Lott_Replication.R
+++ Code/Lott_Replication.R
@@ -6,19 +6,34 @@
 ############################################################################
 CountyCrime = read_dta(here("Data/UpdatedCountyCrimeData-2010.dta"))
 
-CountyCrime %<>% filter(year<1993) %>% arrange(desc(year))
+CountyCrime %<>% filter(year<1993)
 
 CountyCrime_ByYear = CountyCrime %>%
   group_by(year) %>%
-  summarize(Avg_RatMur = mean(ratmur, na.rm=TRUE),
-            Avg_RatRap = mean(ratrap, na.rm=TRUE),
-            Avg_RatAgg = mean(ratagg, na.rm=TRUE))
+  summarize(Avg_RatMur = mean(ratmur))
 
 head(CountyCrime_ByYear, 16)
 
-CountyCrime_ByYear_Line = ggplot(data=CountyCrime_ByYear,mapping=aes(year, Avg_RatMur)) +
+ CountyCrime_ByYear_Line = ggplot(data=CountyCrime_ByYear,mapping=aes(year, Avg_RatMur)) +
   geom_line(mapping=aes(year, Avg_RatMur), col='red') +
-  geom_line(mapping=aes(year, Avg_RatRap), col='blue') +
-  geom_line(mapping=aes(year, Avg_RatAgg), col='blue') +
-  theme_minimal()
+  #geom_line(mapping=aes(year, Avg_RatRap), col='blue') +
+  #geom_line(mapping=aes(year, Avg_RatAga), col='blue') +
+  theme_minimal() +
+  labs(title="Average Murder Rate per county by year")
 CountyCrime_ByYear_Line
+
+#################################################
+#Working through State Level data to check the differences between the state and county (assumed that county has more measurement error)
+
+StateCrime = read_dta(here("Data/UpdatedStateLevelData-2010.dta"))
+
+Murder_ByState_Avg = StateCrime %>%
+  group_by(state, year) %>%
+  summarize(MurRate = murder/(popstate/1000000)) %>%
+  group_by(year) %>%
+  summarize(MurRate_Avg = mean(MurRate))
+
+Murder_ByState_Plot = ggplot(data=Murder_ByState_Avg) +
+  geom_line(aes(year, MurRate_Avg)) +
+  theme_minimal()
+Murder_ByState_Plot
